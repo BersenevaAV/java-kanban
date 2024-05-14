6f3dfc3c7d69d02b8571f29a436e8b9f550cdc13
unknown <BersenevaAV@yandex.ru>
2024-05-15 00:55:57 +0300

    Корректировка стиля InMemoryHistoryManager

--- src/managers/InMemoryHistoryManager.java
+++ src/managers/InMemoryHistoryManager.java
@@ -18,25 +18,22 @@ public class InMemoryHistoryManager implements HistoryManager {
 
     @Override
     public void add(Task task) {
-
         Node newNode = new Node(task);
-
-        if (historyMap.containsKey(task.getId())){
+        if (historyMap.containsKey(task.getId())) {
             removeNode(historyMap.get(task.getId()));
-        }
-        else{
-            if(size == 10) {
+        } else{
+            if (size == 10) {
                 removeNode(head);
                 System.out.println("Удален первый узел");
             }
         }
-
         linkLast(task);
         historyMap.put(task.getId(), newNode);
     }
+
     @Override
     public void remove(int id) {
-        if (historyMap.containsKey(id)){
+        if (historyMap.containsKey(id)) {
             removeNode(historyMap.get(id));
         }
     }
@@ -56,13 +53,12 @@ public class InMemoryHistoryManager implements HistoryManager {
         newNode.prev = tail;
         tail = newNode;
         size++;
-
     }
 
     public void getTasks() {
         historyList.clear();
         Node currentNode = head;
-        while(currentNode!=null){
+        while (currentNode != null) {
             historyList.add(currentNode.data);
             currentNode = currentNode.next;
         }
@@ -70,26 +66,22 @@ public class InMemoryHistoryManager implements HistoryManager {
 
     private void removeNode(Node node) {
         Node currentNode = head;
-
-        if(size!=0){
-            for(int i=0;i<size;i++){
-                if(currentNode.data.getId() == node.data.getId()){
+        if (size!=0) {
+            for (int i=0;i<size;i++) {
+                if (currentNode.data.getId() == node.data.getId()) {
                     Node nextNode = currentNode.next;
                     Node prevNode = currentNode.prev;
-                    if(nextNode == null){
+                    if (nextNode == null) {
                         tail = prevNode;
-                    }
-                    else{
+                    } else {
                         nextNode.prev = prevNode;
                     }
-
-                    if (prevNode == null){
+                    if (prevNode == null) {
                         head = nextNode;
                     }
-                    else{
+                    else {
                         prevNode.next = nextNode;
                     }
-
                     size--;
                     break;
                 }
