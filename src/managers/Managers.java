f50487e6b4e126d54826670180872509dd3677d0
unknown <BersenevaAV@yandex.ru>
2024-05-15 00:34:11 +0300

    Корректировка стиля Managers

--- src/managers/Managers.java
+++ src/managers/Managers.java
@@ -1,18 +1,11 @@
 package managers;
 
-import managers.HistoryManager;
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-
 public class Managers {
-    static TaskManager getDefault(){
+    static TaskManager getDefault() {
         return (new InMemoryTaskManager());
     }
-    static HistoryManager getDefaultHistory(){
+    
+    static HistoryManager getDefaultHistory() {
         return (new InMemoryHistoryManager());
     }
 
