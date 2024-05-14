09388311b02483b229aab1e911a0e24590fcd3ff
unknown <BersenevaAV@yandex.ru>
2024-05-15 00:27:54 +0300

    Корректировка стиля SubTask класса

--- src/tasks/SubTask.java
+++ src/tasks/SubTask.java
@@ -3,9 +3,9 @@ package tasks;
 public class SubTask extends Task {
     private final int idEpic;
 
-    public SubTask(String name, String description, int idEpic){
+    public SubTask(String name, String description, int idEpic) {
         super(name, description);
-        this.idEpic=idEpic;
+        this.idEpic = idEpic;
         super.type = TypeTask.SUBTASK;
     }
 
