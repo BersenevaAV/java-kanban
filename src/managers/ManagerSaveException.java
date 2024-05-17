4389b2d4338ecfcd2551af8458b467b1f7898462
unknown <BersenevaAV@yandex.ru>
2024-05-17 14:44:50 +0300

    Корректировка стиля ManagerSavaException

--- src/managers/ManagerSaveException.java
+++ src/managers/ManagerSaveException.java
@@ -1,10 +1,7 @@
 package managers;
 
-import java.io.IOException;
-
-public class ManagerSaveException extends RuntimeException{
+public class ManagerSaveException extends RuntimeException {
     public ManagerSaveException(final Throwable cause) {
         super(cause);
     }
-
 }
